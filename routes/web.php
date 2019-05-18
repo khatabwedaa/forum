ee1192f7e7570927bcf1b035d312adc0357365d4
MrKhatab <khatabwedaa@gmail.com>
2019-05-18 12:59:55 +0200

    edit some route

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,20 @@
 Auth::routes();
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/threads');
 });
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', function () {
+    return redirect()->route('profile' , auth()->user()->name);
+})->name('home');
 
 Route::get('/threads', 'ThreadsController@index');
-Route::post('/threads', 'ThreadsController@store')->name('threads.store');
+Route::get('/threads/{channel}', 'ThreadsController@index');
 Route::get('/threads/create', 'ThreadsController@create');
+Route::post('/threads', 'ThreadsController@store')->name('threads.store');
 Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
 Route::delete('/threads/{channel}/{thread}', 'ThreadsController@destroy');
-Route::get('/threads/{channel}', 'ThreadsController@index');
+
 Route::post('/threads/{channel}/{thread}/replies' , 'RepliesController@store');
 Route::patch('/replies/{reply}' , 'RepliesController@update');
 Route::delete('/replies/{reply}' , 'RepliesController@destroy');
