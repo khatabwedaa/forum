c8d4c562049bd38faad915a5f5d2694d80a8709a
MrKhatab <khatabwedaa@gmail.com>
2019-06-09 16:26:26 +0200

    add avatar to user profile

---
+++ app/Http/Controllers/Api/UserAvatarController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class UserAvatarController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store()
+    {
+        request()->validate(array(
+            'avatar' => 'required|image',
+        ));
+
+        auth()->user()->update([
+            'avatar_path' => request()->file('avatar')->store('avatars' , 'public')
+        ]);
+    }
+}
--- app/User.php
+++ app/User.php
@@ -11,6 +11,32 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email' , 'password', 'avatar_path'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 
     /**
      * Relation with Thread
@@ -47,33 +73,6 @@ class User extends Authenticatable
     {
         return $this->Threads()->create($thread);
     }
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email' , 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token'
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 
     public function getRouteKeyName()
     {
