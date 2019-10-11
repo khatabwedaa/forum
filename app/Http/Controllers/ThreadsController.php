585de3239a02d7873ef083ba454649744d085f2a
MrKhatab <khatabwedaa@gmail.com>
2019-10-12 01:38:45 +0200

    stop verfy email fix

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -14,7 +14,6 @@ class ThreadsController extends Controller
     public function __construct()
     {
         $this->middleware('auth')->except('index' , 'show');
-        $this->middleware('verified')->only('create' , 'store');
     }
 
     /**
