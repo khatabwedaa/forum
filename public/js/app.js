6b4bca7a558852ae6031a108e916677a515537fa
khatabwedaa <khatabwedaa@gmail.com>
2020-03-28 11:52:49 +0200

    wip

--- public/js/app.js
+++ public/js/app.js
@@ -69595,8 +69595,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/khatabwedaa/Dev/laravel/forum/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/khatabwedaa/Dev/laravel/forum/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/khatabwedaa/Dev/courses/forum/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/khatabwedaa/Dev/courses/forum/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
