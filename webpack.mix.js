e323de32f0a11c0caf012598253f8ec31a36f9eb
khatabwedaa <khatabwedaa@gmail.com>
2020-12-31 19:15:18 +0200

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,4 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .browserSync('localhost:8000');
+    .browserSync('http://forum.test');
